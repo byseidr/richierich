2f4e42daa252b5812a05d19aed7d1b510a4ad4e5
John Anderson Muniz <johnandersonmuniz@gmail.com>
2021-12-09 09:02:20 -0300

    Add getRandomInt and update to version 1.6.0

--- src/index.ts
+++ src/index.ts
@@ -104,6 +104,14 @@ export const getKeyTrue = (
     defaultVal: any = true
 ): true => (hasKeyTrue(el, key) ? el[key] : defaultVal);
 
+// Function taken from:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+export const getRandomInt = (min: number, max: number): number => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+};
+
 export const getSize = (el: any) => {
     if (isObj(el)) {
         el = Object.keys(el).length;
