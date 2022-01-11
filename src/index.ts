136870183c78cd2a5fdafbe980ccd06e00b22d4b
John Anderson Muniz <johnandersonmuniz@gmail.com>
2022-01-11 04:37:40 -0300

    Add shuffleArr

--- src/index.ts
+++ src/index.ts
@@ -357,6 +357,23 @@ export const omit = (obj: { [key: string]: any }, omitKeys: string[]) =>
         .filter((key: string) => !toArr(omitKeys).includes(key))
         .reduce((result, key: string) => ({ ...result, [key]: obj[key] }), {});
 
+export const shuffleArr = (array: any[]): any[] => {
+    let currentIndex: number = array.length,
+        randomIndex: number;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
+    return array;
+};
+
 export const sleep = (ms: number): Promise<number> =>
     new Promise((resolve) => setTimeout(resolve, ms));
 
