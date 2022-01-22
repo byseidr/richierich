4c83040941f0a4857c16b2dc7b8d9b378630a803
John Anderson Muniz <johnandersonmuniz@gmail.com>
2022-01-22 05:48:37 -0300

    Add getRandomEl

--- src/index.ts
+++ src/index.ts
@@ -104,6 +104,11 @@ export const getKeyTrue = (
     defaultVal: any = true
 ): true => (hasKeyTrue(el, key) ? el[key] : defaultVal);
 
+export const getRandomEl = (arr: any[]): any => {
+    const index = getRandomInt(0, arr.length);
+    return getKey(arr, <string>(<any>index));
+};
+
 // Function taken from:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 export const getRandomInt = (min: number, max: number): number => {
