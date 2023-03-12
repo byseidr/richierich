1ca5d293426ce893f673d0acba8e86b178417e64
Seidr <seidr@byseidr.com>
2023-03-12 19:27:18 -0300

    Add RangeWithLeadingZero type

--- src/types.ts
+++ src/types.ts
@@ -17,6 +17,13 @@ export type Range<F extends number, T extends number> =
     | Exclude<Enumerate<T>, Enumerate<F>>
     | T;
 
+export type RangeWithLeadingZero<
+    S extends number,
+    E extends number
+> = E extends Range<0, 9>
+    ? `0${Range<S, E>}`
+    : `${`0${Range<S, 9>}` | `${Range<10, E>}`}`;
+
 export type TupleToUnion<T extends unknown[]> = T[number];
 
 export type ValueOf<T> = T[keyof T];
